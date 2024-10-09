6d1c443bc3ef550c6b740600f4097ac12d433877
chrisonoo <chris@onoo.no>
2024-10-09 21:33:12 +0200

    Update imports, configure Next.js, and improve Tailwind content paths
    Replaced absolute imports with aliases for better consistency. Introduced a Next.js configuration file enabling strict mode. Refined Tailwind CSS content paths to ensure a more comprehensive scan for class names.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/app/**/*.{ts,tsx}', './src/content/**/*.mdx', './src/public/**/*.svg', './app/**/*.{ts,tsx}', './content/**/*.mdx', './public/**/*.svg'],
+  content: [
+    './src/**/*.{ts,tsx}',
+    './src/app/**/*.{ts,tsx}',
+    './src/content/**/*.mdx',
+    './src/public/**/*.svg'],
   theme: {},
   future: {
     hoverOnlyWhenSupported: true,
